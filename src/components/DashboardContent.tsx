6c24f0ddc223dae2b16267a63fb62ef455b6dad2
pankajsir914 <pankajsir914@gmail.com>
2025-08-25 20:07:40 +0530

    Update DashboardContent.tsx

--- src/components/DashboardContent.tsx
+++ src/components/DashboardContent.tsx
@@ -48,7 +48,7 @@ export function DashboardContent() {
     {
       title: "Super Andar Bahar",
       provider: "Evolution",
-      image: "/placeholder.svg", 
+      image: "/lovable-uploads/ab20.jpg", 
       path: "/andar-bahar",
       gradient: "from-red-600 to-red-800"
     },
