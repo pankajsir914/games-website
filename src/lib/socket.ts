8247f531ad0f04e32be527aa18caea4f05c69f09
sovranspatel <120584698+sovranspatel@users.noreply.github.com>
2025-12-21 14:37:31 +0530

    Create socket.ts

---
+++ src/lib/socket.ts
@@ -0,0 +1,16 @@
+// src/lib/socket.ts
+
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket | null = null;
+
+export const getCasinoSocket = () => {
+  if (!socket) {
+    socket = io(import.meta.env.VITE_SOCKET_URL, {
+      path: "/socket.io",
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+  }
+  return socket;
+};
