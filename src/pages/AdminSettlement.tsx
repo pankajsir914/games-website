d64da97c98d8944ca087b16b344577788e8d1080
pankajsir914 <pankajsir914@gmail.com>
2025-12-28 12:39:58 +0530

    Create AdminSettlement.tsx

---
+++ src/pages/AdminSettlement.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AdminLayout } from '@/components/admin/AdminLayout';
+import { SettlementPage } from '@/components/admin/SettlementPage';
+
+const AdminSettlement = () => {
+  return (
+    <AdminLayout>
+      <SettlementPage />
+    </AdminLayout>
+  );
+};
+
+export default AdminSettlement;
+
